0cf317c844284b6658b98b57c8c57e9c7409cb86
Tushar Naik <Tushar Naik@IB-Pune-PC-077>
2019-01-10 14:21:02 +0530

    Added new changes

--- spring-mvc-demo/src/com/luv2code/springdemo/mvc/HelloWorldController.java
+++ spring-mvc-demo/src/com/luv2code/springdemo/mvc/HelloWorldController.java
@@ -2,12 +2,13 @@ package com.luv2code.springdemo.mvc;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequestMapping("/hello")
 public class HelloWorldController {
 
 	// Need a controller method to show the initial form
@@ -35,6 +36,26 @@ public class HelloWorldController {
 		
 		//create the message
 		String result = "YO! " +theName;
+		
+		//add message to model
+		model.addAttribute("message", result);
+		
+		return "helloworld";
+	}
+	
+
+	//Use @RequestParam("StudentName") String theName instead of HttpServletRequest request
+	@RequestMapping("/processFormVersionThree")
+	public String processFormVersionThree(@RequestParam("studentName") String theName, Model model) {
+		
+		//read the request parameter from the HTML form
+		//String theName = request.getParameter("studentName");
+		
+		//convert data to all caps
+		theName = theName.toUpperCase();
+		
+		//create the message
+		String result = "Hey My Friend : " +theName;
 		
 		//add message to model
 		model.addAttribute("message", result);
